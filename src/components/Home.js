9debd711f2fd66c098585f9d87f00090e69c71d0
student <kmac.kimaru93@gmail.com>
2024-04-23 20:45:37 +0300

    adding props basic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function Home(props) {
+  const headStyle = {
+    color: props.color,
+  };
+  
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
+      <h1 style={headStyle}>
         {props.name} is a Web Developer from {props.city}
       </h1>
     </div>
